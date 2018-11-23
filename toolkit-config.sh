b7451064f3844b4e853cfc0b9ce33ab3881f1153
Claes Ladefoged <claesnl@gmail.com>
2018-11-23 17:03:24 +0100

    fixed install path for lib

--- toolkit-config.sh
+++ toolkit-config.sh
@@ -1,5 +1,4 @@
 # rh-script configuration 
 export RH_SCRIPTS=/opt/rhscripts
 export PATH=${RH_SCRIPTS}/bin:${PATH}
-export PYTHONPATH=${RH_SCRIPTS}/lib:${PYTHONPATH}
-export LD_LIBRARY_PATH=${RH_SCRIPTS}/lib:${LD_LIBRARY_PATH}
+export PYTHONPATH=${RH_SCRIPTS}:${PYTHONPATH}
