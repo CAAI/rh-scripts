277ea1d7e18a7fae00186ea68725400bbb31fe9f
Claes Ladefoged <claesnl@gmail.com>
2018-11-28 14:57:13 +0100

    Added combability for UBUNTU pydicom in conversion

--- pythontoolkit/version.py
+++ pythontoolkit/version.py
@@ -1,4 +1,4 @@
-version = '0.0.4'
+version = '0.0.5'
 git_revision = ''
 
 """
@@ -8,6 +8,7 @@ VERSIONING
   0.0.2 # Added mnc2dcm functionality
   0.0.3 # Added comments to pythontoolkit
   0.0.4 # Added utils and updated mnc2dcm
+  0.0.5 # Added combability for UBUNTU pydicom in conversion
 
 """
 
