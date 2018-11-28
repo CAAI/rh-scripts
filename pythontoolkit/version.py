b79d28955c42e32b9c6809247c9a4631e95a2e8e
Claes Ladefoged <claesnl@gmail.com>
2018-11-28 09:45:51 +0100

    Update version.py

--- pythontoolkit/version.py
+++ pythontoolkit/version.py
@@ -1,6 +1,13 @@
 version = '0.0.1'
 git_revision = ''
 
+"""
+
+VERSIONING
+  0.0.1 # Added conversion script and minimal example functions
+
+"""
+
 def __show_version__():
 	print('RH-scripts version: %s' % version)
 	print('Git revision: %s' % git_revision)
