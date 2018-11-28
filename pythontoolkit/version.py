8f3526b30298e6227fabf90eb27d31cd54866f0a
Claes Ladefoged <claesnl@gmail.com>
2018-11-28 16:38:14 +0100

    updated version number

--- pythontoolkit/version.py
+++ pythontoolkit/version.py
@@ -1,4 +1,4 @@
-version = '0.0.5'
+version = '0.0.6'
 git_revision = ''
 
 """
@@ -9,6 +9,7 @@ VERSIONING
   0.0.3 # Added comments to pythontoolkit
   0.0.4 # Added utils and updated mnc2dcm
   0.0.5 # Added combability for UBUNTU pydicom in conversion
+  0.0.6 # Added dicom as toolkit option
 
 """
 
