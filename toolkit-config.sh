b56c2a5cd54ac027f742837f00af8ba1d1b470e3
Claes Ladefoged <claesnl@gmail.com>
2018-11-29 11:32:54 +0100

    Modified CMakeList and config files

--- toolkit-config.sh
+++
@@ -1,5 +0,0 @@
-# rh-script configuration 
-export CAAI=/opt/caai
-
-export PATH=${CAAI}/bin:${PATH}
-export PYTHONPATH=${CAAI}:${PYTHONPATH}
---
+++ toolkit-config.sh.cmake
@@ -0,0 +1,5 @@
+# rh-script configuration 
+export CAAI=@CMAKE_INSTALL_PREFIX@
+
+export PATH=${CAAI}/bin:${PATH}
+export PYTHONPATH=${CAAI}:${PYTHONPATH}
