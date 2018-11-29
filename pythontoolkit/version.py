8b5f89af71cf2abb6a426101d2e9393833215493
Claes Ladefoged <claesnl@gmail.com>
2018-11-29 08:29:06 +0100

    updated version number for toolkit

--- pythontoolkit/version.py
+++ pythontoolkit/version.py
@@ -1,4 +1,4 @@
-version = '0.0.6'
+version = '0.0.7'
 git_revision = ''
 
 """
@@ -10,6 +10,7 @@ VERSIONING
   0.0.4 # Added utils and updated mnc2dcm
   0.0.5 # Added combability for UBUNTU pydicom in conversion
   0.0.6 # Added dicom as toolkit option
+  0.0.7 # Added rtdose_to_mnc and rtx_to_mcn
 
 """
 
